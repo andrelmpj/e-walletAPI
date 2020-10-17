91b5f2841c11ef83180559e309f152d850ac6c5a
Andre Moraes <andrelmpj@Hotmail.com>
2020-10-17 20:13:15 -0300

    Configuracao Inicial de Testes

--- src/test/java/com/info/ewallet/EWalletApplicationTests.java
+++ src/test/java/com/info/ewallet/EWalletApplicationTests.java
@@ -2,12 +2,16 @@ package com.info.ewallet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EWalletApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
